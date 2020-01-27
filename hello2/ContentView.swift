26c3e480341757a8658602cdbe8c32453162a896
m-tange <m-tange@nec.com>
2020-01-27 15:27:33 +0900

    Initial Commit

---
+++ hello2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  hello2
+//
+//  Created by gaziru on 2020/01/27.
+//  Copyright © 2020 gaziru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
